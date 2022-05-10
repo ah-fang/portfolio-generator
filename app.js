2dcc1a46cb8bbbba9c3f5e029aa529a95cf2cad1
ah-fang <94493319+ah-fang@users.noreply.github.com>
2022-05-10 11:11:39 -0700

    initial inquirer prompt

--- app.js
+++ app.js
@@ -1,21 +1,22 @@
-const profileDataArgs = process.argv.slice(2, process.argv.length);
+const inquirer = require("inquirer");
+// const fs = require('fs');
 
-const [name, github] = profileDataArgs;
+// const generatePage = require('./src/page-template.js');
 
-const fs = require('fs');
+// const pageHTML = generatePage(name, github);
 
-const generatePage = require('./src/page-template.js');
+// fs.writeFile("./index.html", pageHTML, err => {
+//     if(err) throw err;
 
-fs.writeFile("index.html", generatePage(name, github), err => {
-    if(err) throw err;
+//     console.log("Portfolio complete! Check out index.html to see the output!");
+// });
 
-
-    console.log("Portfolio complete! Check out index.html to see the output!");
-});
-// console.log(profileDataArgs);
-
-// const printProfileData = profileDataArr => {
-//     profileDataArr.forEach(profileItem => console.log(profileItem))
-// };
-
-// printProfileData(profileDataArgs);
+inquirer
+.prompt([
+    {
+        type: "input",
+        name: "name",
+        message: "What is your name?"
+    }
+])
+.then(answers => console.log(answers));
